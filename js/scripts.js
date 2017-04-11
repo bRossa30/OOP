9437c733665895a5482fd32cd13dfc8287f8925c
Beata <roszkowska.be@gmail.com>
2017-04-11 16:01:13 +0200

    adding board navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(function() {
 
+	var n = 0; //number of board
+
 	function randomString() {
 		var chars = "0123456789abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ",
 			str = '';
@@ -10,6 +12,7 @@ $(function() {
 		return str;
 	}
 
+	// object Column
 	function Column(name) {
 		var self = this;
 		this.id = randomString();
@@ -42,7 +45,6 @@ $(function() {
 				.append($columnAddCard)
 				.append($columnCardList);
 
-
 			return $column;
 		};
 	};
@@ -57,6 +59,7 @@ $(function() {
 		}
 	};
 
+	//object Card
 	function Card(description, color) {
 		var self = this;
 
@@ -65,13 +68,11 @@ $(function() {
 		this.color = color;
 		this.$element = createCard();
 
-
 		function createCard() {
 			var $card = $('<li>').addClass('card'),
 				$cardDescription = $('<textarea>').addClass("card-description").val(self.description),
 				$cardDelete = $('<button>').addClass('btn-delete').text('x'),
 				$cardDeleteDescription = $('<div>').addClass('btn-delete-description hidden').text('Usuń');
-
 
 			$cardDelete.on('click', function() {
 				self.removeCard();
@@ -92,41 +93,34 @@ $(function() {
 		this.$element.remove();
 	}
 
+	//object Board
 	function Board(name) {
 		var self = this;
 		this.id = randomString();
 		this.name = name;
+		this.item = n;
 		this.$element =  createBoard();
 
 		function createBoard() {
-			var $board = $('<div>').addClass('board'),
+			var $board = $('<div>').addClass('board active').attr('data-item', self.item),
 				$boardTitle = $('<h1>').text(self.name),
-				$boardDelete = $('<button>').addClass('btn-delete btn-delete-board').text('x'),
 				$boardAddColumn = $('<button>').addClass('create-column').text('Dodaj kolumnę'),
 				$boardColumnContainer = $('<div>').addClass('column-container');
-
-
-			$boardDelete.on('click', function() {
-				self.removeBoard();
-			});
 
 			$boardAddColumn.on('click', function() {
 				self.addColumn(new Column(prompt("Podaj nazwę kolumny")));
 			});
-
-			$board.append($boardDelete)
-				.append($boardTitle)
+				
+			$board.append($boardTitle)
 				.append($boardAddColumn)
 				.append($boardColumnContainer);
 
 			return $board;
 		};
+
 	};
 
 	Board.prototype = {
-		removeBoard: function() {
-			this.$element.remove();
-		},
 
 		addColumn: function(column) {
 			this.$element.children('.column-container').append(column.$element);
@@ -135,11 +129,6 @@ $(function() {
 
 	};
 
-	$('.create-board').on('click', function() {
-		var board = new Board(prompt('Podaj nazwę tablicy'));
-		$('.create-board').before(board.$element);
-	});
-
 	function initSortable() {
 		$('.column-card-list').sortable({
 			connectWith: '.column-card-list',
@@ -147,12 +136,106 @@ $(function() {
 		}).disableSelection();
 	};
 
-	var board = new Board('Tablica Kanban'),
-		toDoColumn = new Column('Do zrobienia'),
+	//object BoarListElement in board-navigation
+	function BoardListElem(name) {
+		var self = this;
+		this.name = name;
+		this.item = n;
+		this.$element = createListItem();
+
+		function createListItem() {
+			var $li = $('<li>').addClass('active').attr('data-li_item', self.item);
+				$liDescription = $('<p>').text(self.name);
+				$liDelete = $('<button>').addClass('btn-delete').text('x');
+
+			$li.append($liDescription)
+				.append($liDelete);
+
+			return $li;
+		}
+
+		$liDelete.on('click', function() {
+			self.removeListElem();
+		});
+
+		this.$element.on('click', function() {
+			addActiveClassLi(self.item);
+			addActiveClassBoard(self.item);
+			removeActiveClassLi(self.item);
+			removeActiveClassBoard(self.item);
+		});
+	};
+
+	BoardListElem.prototype.removeListElem = function() {
+		var x = this.$element.attr('data-li_item');
+		console.log(x);
+		this.$element.remove();
+		$('.board-container').children('[data-item=' + x + ']' ).remove();
+		removeActiveClassLi(x);
+		removeActiveClassBoard(x);
+		$('.board-container').children(":first").addClass('active');
+		$('.board-list').children(":first").addClass('active');
+	};
+
+	//class active switching
+	function removeActiveClassLi(x) {
+		$('.board-list').children('[data-li_item!=' + x + ']').removeClass('active');
+	};
+
+	function removeActiveClassBoard(x) {
+		$('.board-container').children('[data-item!=' + x + ']').removeClass('active');
+	};
+
+	function addActiveClassLi(x) {
+		$('.board-list').children('[data-li_item=' + x + ']').addClass('active');
+	};
+
+	function addActiveClassBoard(x) {
+		$('.board-container').children('[data-item=' + x + ']').addClass('active');
+	};
+
+	//creating new Board
+	function createBoardItem(name) {
+		n++;
+		
+		var	board = new Board(name),
+			liElem = new BoardListElem(name);
+
+		$('.board-container').prepend(board.$element);
+		$('.board-list').prepend(liElem.$element);
+
+		removeActiveClassLi(n);
+		removeActiveClassBoard(n);
+
+		return board;
+	}
+
+	$('.create-board').on('click', function() {
+		var name = prompt('Podaj nazwę tablicy');
+
+		createBoardItem(name);
+	});
+
+	//show and hide board navigation
+	$('i.fa-chevron-right').on('click', function() {
+		$('.navigation').animate({width: "100%"});
+		$('.board-container').css({width: "calc(100% - 300px)"});
+		$(this).after($('i.fa-chevron-left'));
+	});
+
+	$('i.fa-chevron-left').on('click', function() {
+		$('.navigation').animate({width: "25px"}).children('ul');
+		$('.board-container').css({width: "calc(100% - 25px)"});
+		$(this).after($('i.fa-chevron-right'));
+	});
+
+	//initialize default board
+	var board = createBoardItem('Tablica Kanban');
+
+	var	toDoColumn = new Column('Do zrobienia'),
 		doingColumn = new Column('W trakcie'),
 		doneColumn = new Column('Skończone');
 
-	$('.create-board').before(board.$element);
 	board.addColumn(toDoColumn);
 	board.addColumn(doingColumn);
 	board.addColumn(doneColumn);
@@ -163,6 +246,7 @@ $(function() {
 	toDoColumn.addCard(card1);
 	doingColumn.addCard(card2);
 
+	//show and hide tbn-delete-description
 	$('.btn-delete').on('mouseover', function() {
 		$(this).next('.btn-delete-description').removeClass('hidden');
 	});
